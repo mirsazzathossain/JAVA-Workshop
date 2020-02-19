b906fc78ec15878eb4d2c216eabb7c997ff855c7
mirsazzathossain <mirsazzathossain@gmail.com>
2020-02-20 02:55:49 +0600

    06 February 2020

--- Lecture-01/src/lecture/myfirstcode/MyFirstCode.java
+++ Lecture-01/src/lecture/myfirstcode/MyFirstCode.java
@@ -16,6 +16,8 @@ public class MyFirstCode {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello World!");
     }
     
 }
